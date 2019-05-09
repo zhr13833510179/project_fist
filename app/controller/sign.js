097226ab99f9a4a7b164b248b01c6daa7954b919
WeiZhiTaiYi <1687925817@qq.com>
2019-05-09 09:33:21 +0800

    添加登录验证

--- app/controller/sign.js
+++ app/controller/sign.js
@@ -4,13 +4,32 @@ const Controller = require('egg').Controller;
 
 class SignController extends Controller {
   async index() {
-    await this.ctx.render('sign.html',{
-        user: "用户名",
-        password_box: "密码",
-        sign_in: "登录",
-        register: "注册用户",
-        forget: "忘记密码"
+    const username = this.ctx.request.body.username;
+    const password = this.ctx.request.body.password;
+    await this.ctx.render('sign.html', {
+      user: "用户名",
+      password_box: "密码",
+      sign_in: "登录",
+      register: "注册用户",
+      forget: "忘记密码"
     })
+  }
+
+  async login() {
+    const username = this.ctx.request.body.username;
+    const password = this.ctx.request.body.password;
+    if (username === "123" && password === "456") {
+      await this.ctx.redirect("/addSafefive")
+    } else {
+      await this.ctx.redirect("/awc")
+    }
+  }
+
+  async cg() {
+    await this.ctx.render("cg.html")
+  }
+  async awc() {
+    await this.ctx.render("awc.html")
   }
 }
 
